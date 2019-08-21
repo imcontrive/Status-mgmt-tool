f9d103741cadf3e2a4bd14e7803236e0aa2e8135
shubh <psubham94@gmail.com>
2019-08-21 13:02:46 +0530

    initial commit

---
+++ client/components/HomePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Status Reports Management (SRM) tool</h1>
+    </div>
+  );
+}
+
+export default HomePage;
